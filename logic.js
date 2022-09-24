41f7ce2f79124b977f82756a766797a384151917
rustam <rustammajid76@gmail.com>
2022-09-24 20:58:14 +0800

    update

--- logic.js
+++ logic.js
@@ -1,13 +1,30 @@
 // test
 
+// do while
 let i = 1;
 
-do{
+do {
+  if (i % 2 === 0) console.log(i);
+  i++;
+} while (i <= 20);
 
- if(i % 2 === 0)
-    console.log(i)
-    i++;
+console.log(i);
 
-}while(i <= 20)
+console.log("batas ");
 
-console.log(i)
+//while
+let a = 1;
+
+while (a <= 20) {
+  if (a % 2 === 0) console.log(a);
+  a++;
+}
+
+console.log("batas");
+
+// for loop
+
+for (let b = 20; b >= 1; b--) {
+    if(b % 2 === 0)
+        console.log(b);
+}
